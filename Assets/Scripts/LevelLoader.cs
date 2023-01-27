6828869dfdfa2156b16144a3e87286d376b0eb04
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-01-27 10:15:44 -0500

    File Organization, Title screen, Transitions, MainMenu,, and Fonts
    This commit adds a basic title screen (NOT FINAL) and adds a fade in and out effect when switching scenes.  Also adds a main Menu Scene (It has nothing yet) and organized a few files.
    Added a (maybe) temporary font called "Rumble Tumble", further use of it should be discussed.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+   
+   public Animator transition;
+
+   public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+
+       if(Input.GetMouseButtonDown(0)) 
+       {
+            //If player imuts a mouse click it loads the next scene
+            LoadNextLevel();
+       }
+
+    }
+
+    public void LoadNextLevel()
+       {
+
+        //Method that checks for the next scene that is loaded
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+       }
+
+       IEnumerator LoadLevel(int levelIndex)
+       {
+            transition.SetTrigger("Start");
+
+            yield return new WaitForSeconds(transitionTime);
+            SceneManager.LoadScene(levelIndex);
+       }
+}
