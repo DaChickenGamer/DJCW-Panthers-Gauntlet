cb7323bbbd58a4c08cb83e57f85982a0b2571da3
ChickenGamer <rjmichalski2006@gmail.com>
2023-02-15 08:29:30 -0500

    Kicking Animations & Fixed Attacks
    Made it so we now have kicking animations and the attacks now work without errors.

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -8,8 +8,8 @@ public class Combat : MonoBehaviour
 {
     private Animator animator;
 
-    private Vector2 kickValue;
-    private Vector2 punchValue;
+    private float kickValue;
+    private float punchValue;
 
     private void Awake()
     {
@@ -18,13 +18,10 @@ public class Combat : MonoBehaviour
     }
     private void OnPunch(InputValue value)
     {
-        punchValue = value.Get<Vector2>();
+        punchValue = value.Get<float>();
 
-        if (punchValue.x != 0 || punchValue.y != 0)
+        if (punchValue != 0)
         {
-            animator.SetFloat("X", punchValue.x);
-            animator.SetFloat("Y", punchValue.y);
-
             animator.SetBool("isPunching", true);
         }
         else
@@ -35,13 +32,10 @@ public class Combat : MonoBehaviour
     }
     private void OnKick(InputValue value)
     {
-        kickValue = value.Get<Vector2>();
+        kickValue = value.Get<float>();
 
-        if (kickValue.x != 0 || kickValue.y != 0)
+        if (kickValue != 0)
         {
-            animator.SetFloat("X", kickValue.x);
-            animator.SetFloat("Y", kickValue.y);
-
             animator.SetBool("isKicking", true);
         }
         else
