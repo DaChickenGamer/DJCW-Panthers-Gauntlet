9172d5b272e4e619c99dcc77f541a22767e0a9d4
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-02-16 09:54:59 -0500

    Gamestages
    Added the code the other other commit could of had

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    public GameState state;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start() 
+    {
+        UpdateGameState(GameState.Tutorial);
+    }
+    
+    public void UpdateGameState(GameState newState)
+    {
+        state = newState;
+
+        switch(newState)
+        {
+            case GameState.Tutorial:
+                break;
+                HandleTutorial();
+            case GameState.InRing:
+                break;
+                HandleInRing();
+            case GameState.PlayerLose:
+                break;
+                HandlePlayerLose();
+            case GameState.PlayerWin:
+                break;
+                HandlePlayerWin();
+            default:
+            throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    private void HandleTutorial()
+    {
+
+    }
+
+    private void HandleInRing()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandlePlayerLose()
     {
         
+    }
+
+    private void HandlePlayerWin()
+    {
+        
+    }
+
+    public enum GameState
+    {
+        Tutorial,
+        InRing,
+        PlayerLose,
+        PlayerWin
     }
 }
