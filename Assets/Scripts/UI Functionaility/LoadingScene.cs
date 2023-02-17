8e1e303a6e8554326d73aa4b9c21349455c428d9
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-02-17 10:49:37 -0500

    LoadingScreen
    Adds as loading screen

---
+++ Assets/Scripts/UI Functionaility/LoadingScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+   public GameObject LoadingScreen;
+   public Image LoadingBarFill;
+
+   public void loadscene(int sceneId)
+   {
+        StartCoroutine(LoadSceneAsync(sceneId));
+   }
+
+   IEnumerator LoadSceneAsync(int sceneId)
+   {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        while(!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);        
+
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+   }
+}
