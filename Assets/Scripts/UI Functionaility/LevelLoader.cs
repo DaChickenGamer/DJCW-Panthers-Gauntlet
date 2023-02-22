0ba8f7a06e2071f5eb18ef4b31803940ae0449c6
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-02-22 10:25:31 -0500

    Working loading screen

--- Assets/Scripts/UI Functionaility/LevelLoader.cs
+++ Assets/Scripts/UI Functionaility/LevelLoader.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
-   
+   public GameObject LoadingScreen;
+   public Image LoadingBarFill;
    public Animator transition;
 
    private static float transitionStop = 0f;
@@ -25,24 +27,45 @@ public class LevelLoader : MonoBehaviour
       }
    }
 
-   IEnumerator LoadLevel(int levelIndex)
+   public void loadscene(int sceneId)
    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+   }
+
+
+   IEnumerator LoadLevel(int sceneId)     //Level transition
+   {
+      AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+      
       if (transitionStop == 0)
       {
          transition.SetTrigger("Start");
 
          yield return new WaitForSeconds(transitionTime);
-         SceneManager.LoadScene(levelIndex);
+         SceneManager.LoadScene(sceneId);
       }
    }
 
-   public void QuitGame()
+   IEnumerator LoadSceneAsync(int sceneId)      //Loading screen
+   {
+      AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+      LoadingScreen.SetActive(true);
+      while(!operation.isDone)
+      {
+         float progressValue = Mathf.Clamp01(operation.progress / 0.9f);        
+
+         LoadingBarFill.fillAmount = progressValue;
+
+         yield return null;
+      }
+   }
+
+   public void QuitGame()     //Quit game
    {
       Application.Quit();
    }
 
-   public void NextScene()
-   {
-      
-   }
+
+   
 }
