233c72628e339e5f681839852fddc42c66e1c2e8
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-03-07 08:16:34 -0500

    Interactable ExitDoor
    Allows player to press the enter key to interact with the door where an overlay is displayed where it asks the player if they want to go to 'Enter the Ring' or 'Stay in the Gym'.

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -8,20 +8,34 @@ public class ExitDoor : MonoBehaviour
 {
     [Header("Door Popup")]
     [SerializeField] private UnityEvent customEvent;
-    
+    private bool enterDoor = false;
     private void OnEnterDoor(InputValue value)
     {
-        Debug.Log("Door Popup Menu");
+        if(enterDoor == true)
+        {
+            Debug.Log("Door Popup Menu");
+            customEvent.Invoke();
+
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collider) 
     {
-        if(collider.gameObject.tag == "Player")
+        if(collider.gameObject.name == "ExitDoor")
         {
-        customEvent.Invoke();
-        Debug.Log("Door Popup Menu");
+             Debug.Log("Enter Door");
+            enterDoor = true;
         }
-        
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if(collider.gameObject.name == "ExitDoor")
+        {
+             Debug.Log("Exit Door");
+            enterDoor = false;
+        }
     }
 
 }
