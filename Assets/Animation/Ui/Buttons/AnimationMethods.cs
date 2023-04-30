d59a0dc2e6a03e5e86bbecb541aaa02a5de35de1
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-04-30 14:35:48 -0400

    Main Menu and Settings Menu
    Added correct logic to the AnimationMethods Script, and added more to the settings menu

--- Assets/Animation/Ui/Buttons/AnimationMethods.cs
+++ Assets/Animation/Ui/Buttons/AnimationMethods.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationMethods : MonoBehaviour
+public class AnimationMethods : MainMenu
 {
     
     public Rigidbody2D rb;
@@ -13,9 +13,19 @@ public class AnimationMethods : MonoBehaviour
     [SerializeField] private float positionIncrement;   //The Amount that adds on to the startingPosition to get the endPosition
     private float endingPosition;   //Stores Object's End Location
     private float currentPosition;  //Stores Current location
+
+    private bool MoveRight, MoveLeft, checkedStartingPosition = false;
+
+
+
     private Animator anim;
     
-    void Update() 
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    private void Timer()
     {
         if(StopMotionTimer>0)
         {
@@ -23,48 +33,67 @@ public class AnimationMethods : MonoBehaviour
         }
         if(StopMotionTimer<=0)
         rb.velocity = new Vector2(0, 0);
-        
-        currentPosition = transform.position.x;
     }
 
-    private void Awake() 
+    //Stores starting positon and then calculates end position
+    private void CheckPositon()
     {
-        startingPosition = transform.position.x;
+        if(checkedStartingPosition == false)
+        {
+            startingPosition = transform.position.x;
+            endingPosition = startingPosition += positionIncrement;
+            Debug.Log("StartingPosition: " + startingPosition + " EndingPosition: " + endingPosition);
+
+            //checkedStartingPosition = true;
+        }
     }
 
-    void Start()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        //Stores starting positon and then calculates end position
-        endingPosition = startingPosition += positionIncrement;
-        Debug.Log("StartingPosition: " + startingPosition + " EndingPosition: " + endingPosition);
-    }
     public void MoveObjectRight()
     {
-        if(currentPosition > endingPosition)
-        {
-            rb.velocity = new Vector2(0, 0);
-        }   
-        else
-        {
-            rb.velocity = new Vector2(x, 0);
-            StopMotionTimer=3;
-            Debug.Log("MoveRight");   
-        }
+        MoveRight = true;
+        MoveLeft = false;
     }
 
     public void MoveObjectLeft()
     {
-        Debug.Log("MoveLeft");
-        if(currentPosition > startingPosition)
-        {
-            rb.velocity = new Vector2(-x, 0);
-            StopMotionTimer=3;     
-        }   
-        else
-        {
-            rb.velocity = new Vector2(0, 0);
-        }
+        MoveRight = false;
+        MoveLeft = true;
     }
     
+    void Update() 
+    {
+        if(animationComplete == true)
+        {   
+            Debug.Log("Joe");
+            CheckPositon();
+            Timer();
+        
+            currentPosition = transform.position.x;
+
+            //Move Left Detection
+            if(MoveLeft == true & (currentPosition <= startingPosition))
+            {
+   
+                rb.velocity = new Vector2(0, 0);
+            }   
+            else
+            {
+                 Debug.Log("MoveLeft");
+                rb.velocity = new Vector2(-x, 0);
+                StopMotionTimer=3;  
+            }
+            
+            //MoveRight Dectection
+            if(MoveRight == true & (currentPosition >= endingPosition))
+            {
+                rb.velocity = new Vector2(0, 0);
+            }   
+            else
+            {
+                rb.velocity = new Vector2(x, 0);
+                StopMotionTimer=3;
+                Debug.Log("MoveRight");   
+            }
+        }
+    }
 }
--- Assets/Scripts/UI Functionaility/MainMenu.cs
+++ Assets/Scripts/UI Functionaility/MainMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+   public bool animationComplete = false;
    public void PlayGame()
    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void AnimationFinished()
+   {
+      animationComplete = true;
+      Debug.Log("Animation Complete" + animationComplete);
    }
 }
