9a20e38c8fbf818163755f441651a063711d8a2f
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-28 12:01:02 -0400

    Behavior Tree
    Adds four scripts (Node, Tree, Selector, and Sequence) that allow for the creation and use of different AI behaviors. Link to video: https://youtu.be/aR6wt5BlE-E?si=2nOu_POLfmTvqAUu
    talks about the code and concepts.

---
+++ Assets/Scripts/Enemy/BehaviorTree/Selector.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BehaviorTree
+{
+    public class Selector : Node
+    {
+        public Selector() : base() { }
+        public Selector(List<Node> children) : base(children) { }
+        public override NodeState Evaluate()
+        {
+            foreach (Node node in children)
+            {
+                switch (node.Evaluate())
+                {
+                    case NodeState.FAILURE:
+                        continue;
+                    case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
+                        continue;
+                    case NodeState.RUNNING:
+                        state = NodeState.RUNNING;
+                        continue;
+                    default:
+                        return state;
+                }
+            }
+            state = NodeState.FAILURE;
+            return state;
+
+        }
+    }
+
+}
