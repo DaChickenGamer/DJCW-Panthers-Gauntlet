c5ce5b9bc37ef624ba1fbc680557ca9461c172dc
ChickenGamer <rjmichalski2006@gmail.com>
2023-10-07 15:19:20 -0400

    Dialogue Character By Character
    The dialogue now looks like it gets typed out because it goes character by character instead of putting the whole string at once.

--- Assets/InteractionSystem/Dialogue.cs
+++ Assets/InteractionSystem/Dialogue.cs
@@ -21,6 +21,7 @@ public class Dialogue : MonoBehaviour, IInteractable
     private bool skip = false;
     private bool tutorialComplete = false;
     private bool inCoachArea = false;
+    public static bool InCoachArea;
     private bool allowNextLine;
     private int index;
 
@@ -106,6 +107,7 @@ public class Dialogue : MonoBehaviour, IInteractable
     }
     private void Update()
     {
+        InCoachArea = inCoachArea; // I can't edit it but I can see the current state
         //CoachTutorial();
     }
 
--- Assets/Scripts/Player/Tutorial.cs
+++ Assets/Scripts/Player/Tutorial.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -35,6 +36,9 @@ public class Tutorial : MonoBehaviour
     private bool flashOn = false; // For flashing
 
     private bool isFlashing  = false;
+    private bool isTyping = false;
+
+    private int CurrentDialogue;
 
     // Colors For Flashing
 
@@ -42,10 +46,12 @@ public class Tutorial : MonoBehaviour
     private Color32 transparentColor = new Color32(255, 255, 255, 150);
 
 
-    // Testing Keybind To Image
-
-    [SerializeField] private InputActionReference inputActionReference;
-    private InputAction inputAction;
+    // Keybinds 
+    [Header("Keybinds")]
+    [SerializeField] private InputActionReference movementInputActionReference;
+    [SerializeField] private InputActionReference interactionInputActionReference;
+    private InputAction movementInputAction;
+    private InputAction interactionInputAction;
 
     /*
     Tutorial Draft:
@@ -62,34 +68,45 @@ public class Tutorial : MonoBehaviour
     {
         playerDirection = PlayerMovement.direction;
 
-        inputAction = inputActionReference.action;
+        movementInputAction = movementInputActionReference.action;
+        interactionInputAction = interactionInputActionReference.action;
 
         MovementFlashingKeys();
 
-        if (Dialogue.metCoach == false && !skipTutorialPopup.activeSelf)
+        if (Dialogue.metCoach == false && !skipTutorialPopup.activeSelf && !isTyping)
         {
             tutorialDialogueBox.SetActive(true);
-            DialogueStorage(1);
+            if (!(DialogueStorage(CurrentDialogue) == tutorialDialogueBox.GetComponentInChildren<TextMeshProUGUI>().text)) // Checks to see if the current dialouge is the same has the text component
+                StartCoroutine(TypeLine(DialogueStorage(CurrentDialogue)));
         }
     }
     private string DialogueStorage(int currentDialogue)
     {
+        currentDialogue = CurrentDialogue; // So we can change the currentDialogue from other methods
+        // Too add more you make a new case and you have to make sure the method is a string
         switch(currentDialogue)
         {
-            case 1:
+            case 0:
                 return MovementCheck();
+            case 1:
+                return ExampleDialogue();
+            case 2:
+                return TalkToCoachCheck();
             default:
                 return "No more text";
         }
     }
-    
+
     /*
         Pure Text but we'll need
 
         - KO Bar Explaination // Step 7
         - KO Meter Explaniation // Step 3
     */
-
+    private string ExampleDialogue()
+    {
+        return "Welcome Adventurer";
+    }
     private string MovementCheck() // Step 1
     {
         if (playerDirection.y > 0)
@@ -113,30 +130,43 @@ public class Tutorial : MonoBehaviour
             flashingKeyLeftImage.SetActive(false);
         }
 
-        return "Null";
+        return "Movement Check Placeholder";
     }
     private void MovementFlashingKeys()
     {
-        if ((!movedUp || !movedDown || !movedLeft || !movedRight) && !isFlashing) // Makes keys flash if not true
+        // Need to tell the player how to move
+
+        if ((!movedUp || !movedDown || !movedLeft || !movedRight) && isFlashing == false) // Makes keys flash if not true
         {
             StartCoroutine(FlashKeys());
+        }
+        else if (isFlashing == false)
+        {
+            CurrentDialogue = 2;
         }
     }
     private string TalkToCoachCheck() // Step 2
     {
-        return "Null";
+        // Tells the player how to interact ( interact with coach )
+
+        if (Dialogue.InCoachArea && interactionInputAction.triggered)
+        {
+            PunchingBagPunchedCheck();
+        }
+
+        return "Talk to coach placeholder";
     }
-    private string PunchingBagPunchedCheck() // Step 4
+    private void PunchingBagPunchedCheck() // Step 4
     {
-        return "Null";
+
     }
-    private string PunchingBagKickedCheck() // Step 5
+    private void PunchingBagKickedCheck() // Step 5
     {
-        return "Null";
+
     }
-    private string GrappledCheck() // Step 6
+    private void GrappledCheck() // Step 6
     {
-        return "Null";
+
     }
 
     private IEnumerator FlashKeys()
@@ -149,10 +179,10 @@ public class Tutorial : MonoBehaviour
         // When we support multiple platforms we can also add a variable for platform
         // The variable will be the folder of the platform name
 
-        flashingKeyUpImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + inputAction.GetBindingDisplayString()[0] + "_Pressed.png");
-        flashingKeyDownImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + inputAction.GetBindingDisplayString()[4] + "_Pressed.png");
-        flashingKeyLeftImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + inputAction.GetBindingDisplayString()[2] + "_Pressed.png");
-        flashingKeyRightImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + inputAction.GetBindingDisplayString()[6] + "_Pressed.png");
+        flashingKeyUpImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + movementInputAction.GetBindingDisplayString()[0] + "_Pressed.png");
+        flashingKeyDownImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + movementInputAction.GetBindingDisplayString()[4] + "_Pressed.png");
+        flashingKeyLeftImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + movementInputAction.GetBindingDisplayString()[2] + "_Pressed.png");
+        flashingKeyRightImage.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Art/Sprites/KeyBinds/Keyboard/Keyboard_" + movementInputAction.GetBindingDisplayString()[6] + "_Pressed.png");
 
         if (flashOn)
         {
@@ -175,4 +205,18 @@ public class Tutorial : MonoBehaviour
 
         isFlashing = false;
     }
+    private IEnumerator TypeLine(string dialogue)
+    {
+        isTyping = true;
+        tutorialDialogueBox.GetComponentInChildren<TextMeshProUGUI>().text = ""; // Clear the text
+
+        for (int i = 0; i < dialogue.Length; i++)
+        {
+            tutorialDialogueBox.GetComponentInChildren<TextMeshProUGUI>().text += dialogue[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        isTyping = false; // Set to false when typing is complete
+    }
+
 }
