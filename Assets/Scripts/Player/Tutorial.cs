543ea6af5ceab68069ee61185fc490e8b9215378
ChickenGamer <rjmichalski2006@gmail.com>
2023-10-06 20:30:21 -0400

    Flash Keys Without Keybinds
    Setup the flashing key system but it doesn't yet show the correct keybinds.

--- Assets/Scripts/Player/Tutorial.cs
+++ Assets/Scripts/Player/Tutorial.cs
@@ -1,11 +1,43 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
+
+    // Popup and Actual box for the tutorial
     public GameObject tutorialDialogueBox;
     public GameObject skipTutorialPopup;
+
+    private Vector2 playerDirection;
+
+    // Different key gameobjects
+    [Header("Flashing Key Objects")]
+    [SerializeField] private GameObject flashingKeyUpImage;
+    [SerializeField] private GameObject flashingKeyDownImage;
+    [SerializeField] private GameObject flashingKeyLeftImage;
+    [SerializeField] private GameObject flashingKeyRightImage;
+
+
+    // Booleans For Checking
+    private bool movedUp = false;
+    private bool movedDown = false;
+    private bool movedLeft = false;
+    private bool movedRight = false;
+
+    private bool flashOn = false; // For flashing
+
+    private bool isFlashing  = false;
+
+    // Colors For Flashing
+
+    private Color32 normalColor = new Color32(255, 255, 255, 255);
+    private Color32 transparentColor = new Color32(255, 255, 255, 150); 
+
 
     /*
     Tutorial Draft:
@@ -22,7 +54,11 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Dialogue.metCoach == false && !skipTutorialPopup.active)
+        playerDirection = PlayerMovement.direction;
+
+        MovementFlashingKeys();
+
+        if (Dialogue.metCoach == false && !skipTutorialPopup.activeSelf)
         {
             tutorialDialogueBox.SetActive(true);
             DialogueStorage(1);
@@ -48,7 +84,35 @@ public class Tutorial : MonoBehaviour
 
     private string MovementCheck() // Step 1
     {
+        if (playerDirection.y > 0)
+        {
+            movedUp = true;
+            flashingKeyUpImage.SetActive(false);
+        }
+        if (playerDirection.y < 0)
+        {
+            movedDown = true;
+            flashingKeyDownImage.SetActive(false);
+        }
+        if (playerDirection.x > 0) 
+        {
+            movedRight = true;
+            flashingKeyRightImage.SetActive(false);
+        }
+        if (playerDirection.x < 0)
+        {
+            movedLeft = true;
+            flashingKeyLeftImage.SetActive(false);
+        }
+
         return "Null";
+    }
+    private void MovementFlashingKeys()
+    {
+        if ((!movedUp || !movedDown || !movedLeft || !movedRight) && !isFlashing) // Makes keys flash if not true
+        {
+            StartCoroutine(FlashKeys());
+        }
     }
     private string TalkToCoachCheck() // Step 2
     {
@@ -65,5 +129,31 @@ public class Tutorial : MonoBehaviour
     private string GrappledCheck() // Step 6
     {
         return "Null";
+    }
+
+    private IEnumerator FlashKeys()
+    {
+        isFlashing = true;
+
+        if (flashOn)
+        {
+            flashingKeyUpImage.GetComponent<Image>().color = normalColor;
+            flashingKeyDownImage.GetComponent<Image>().color = normalColor;
+            flashingKeyLeftImage.GetComponent<Image>().color = normalColor;
+            flashingKeyRightImage.GetComponent<Image>().color = normalColor;
+            flashOn = false;
+        }
+        else
+        {
+            flashingKeyUpImage.GetComponent<Image>().color = transparentColor;
+            flashingKeyDownImage.GetComponent<Image>().color = transparentColor;
+            flashingKeyLeftImage.GetComponent<Image>().color = transparentColor;
+            flashingKeyRightImage.GetComponent<Image>().color = transparentColor;
+            flashOn = true;
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        isFlashing = false;
     }
 }
