e59aa8018bbc599833294f7561daa03819b13436
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-10-09 12:39:48 -0400

    Coach Dialogue
    Added a few lines of dialogue to coach in the Tutorial script explaining the button inputs.

--- Assets/Scripts/UI Functionaility/SkipTutorialCode.cs
+++ Assets/Scripts/UI Functionaility/SkipTutorialCode.cs
@@ -9,7 +9,7 @@ public class SkipTutorialCode : MonoBehaviour
         // Makes the Skip Tutorial no longer visible if you already talked to the coach
         // Also makes it so we can hide it in the inspector
 
-        if (Dialogue.metCoach == false)
+        if (CoachDialogue.metCoach == false)
             transform.GetChild(0).gameObject.SetActive(true);
         else
             transform.GetChild(0).gameObject.SetActive(false);
