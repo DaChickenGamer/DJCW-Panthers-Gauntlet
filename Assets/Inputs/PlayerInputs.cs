57d83eb040f7295f4c7eb7a1cc1279917ae767dc
ChickenGamer <rjmichalski2006@gmail.com>
2023-12-06 13:30:57 -0500

    Merge Keybinding To Main
    This puts everything from the keybinding into main. This includes a working keybinding system and keybinding menu. This has a few other small changes such has organization and a few bug fixes.

--- Assets/Inputs/PlayerInputs.cs
+++ Assets/Inputs/PlayerInputs.cs
@@ -80,15 +80,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""Return"",
-                    ""type"": ""Button"",
-                    ""id"": ""aad0537d-00c8-470a-97f3-708b79ed21c9"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -311,17 +302,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
                     ""action"": ""Pause"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""f2c7fa79-363e-4971-bf20-30f93dcf0be6"",
-                    ""path"": ""<Keyboard>/enter"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Return"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -336,7 +316,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
         m_InGame_Grapple = m_InGame.FindAction("Grapple", throwIfNotFound: true);
         m_InGame_Interact = m_InGame.FindAction("Interact", throwIfNotFound: true);
         m_InGame_Pause = m_InGame.FindAction("Pause", throwIfNotFound: true);
-        m_InGame_Return = m_InGame.FindAction("Return", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -404,7 +383,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
     private readonly InputAction m_InGame_Grapple;
     private readonly InputAction m_InGame_Interact;
     private readonly InputAction m_InGame_Pause;
-    private readonly InputAction m_InGame_Return;
     public struct InGameActions
     {
         private @PlayerInputs m_Wrapper;
@@ -415,7 +393,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
         public InputAction @Grapple => m_Wrapper.m_InGame_Grapple;
         public InputAction @Interact => m_Wrapper.m_InGame_Interact;
         public InputAction @Pause => m_Wrapper.m_InGame_Pause;
-        public InputAction @Return => m_Wrapper.m_InGame_Return;
         public InputActionMap Get() { return m_Wrapper.m_InGame; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -443,9 +420,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
             @Pause.started += instance.OnPause;
             @Pause.performed += instance.OnPause;
             @Pause.canceled += instance.OnPause;
-            @Return.started += instance.OnReturn;
-            @Return.performed += instance.OnReturn;
-            @Return.canceled += instance.OnReturn;
         }
 
         private void UnregisterCallbacks(IInGameActions instance)
@@ -468,9 +442,6 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
             @Pause.started -= instance.OnPause;
             @Pause.performed -= instance.OnPause;
             @Pause.canceled -= instance.OnPause;
-            @Return.started -= instance.OnReturn;
-            @Return.performed -= instance.OnReturn;
-            @Return.canceled -= instance.OnReturn;
         }
 
         public void RemoveCallbacks(IInGameActions instance)
@@ -496,6 +467,5 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
         void OnGrapple(InputAction.CallbackContext context);
         void OnInteract(InputAction.CallbackContext context);
         void OnPause(InputAction.CallbackContext context);
-        void OnReturn(InputAction.CallbackContext context);
     }
 }
